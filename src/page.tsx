e0d330af98f7853b43e6e44730443684fbba7f9d
kyungpyo.kim <kimkyungpyo@nativebinary.com>
2022-10-09 12:56:52 +0900

    Extract <HomeScreen />

--- src/page.tsx
+++ src/page.tsx
@@ -2,14 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { Text, View } from 'react-native';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
+import HomeScreen from './pages/home';
 
 function SettingsScreen() {
   return (
