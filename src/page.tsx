7b74ddb3729227046aa596e2077c2925172de62a
kyungpyo.kim <kimkyungpyo@nativebinary.com>
2022-10-09 15:23:26 +0900

    SetUp stack navigation

--- src/page.tsx
+++ src/page.tsx
@@ -1,8 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { Text, View } from 'react-native';
 import SignIn from './pages/signIn';
+import SignUp from './pages/signup';
 
 function HomeScreen() {
   return (
@@ -17,14 +19,28 @@ function SettingsScreen(): React.ReactElement {
 }
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function TabScreen() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Tab">
+        <Stack.Screen name="Tab" component={TabScreen} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
