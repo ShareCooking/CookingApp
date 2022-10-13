d7df3fa5da31d4e1e81d6e00f2d9513064d6bbbd
kyungpyo.kim <kimkyungpyo@nativebinary.com>
2022-10-13 23:00:07 +0900

    Apply useForm

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,22 +1,56 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { View } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 
 export default function SignIn() {
   const navigation = useNavigation();
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      id: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(data: { id: string; password: string; }) {
+    console.log('onSubmit', data);
+  }
 
   return (
     <View style={{ padding: 10, justifyContent: 'center', height: '100%' }}>
-      <TextInput label="ID / e-mail" />
-      <TextInput style={{ marginTop: 10 }} label="Password" secureTextEntry />
-      <Button style={{ marginTop: 10 }} mode="outlined">
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput label="ID / e-mail"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="id"
+      />
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput style={{ marginTop: 10 }} label="Password" secureTextEntry
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="password"
+      />
+      <Button style={{ marginTop: 10 }} mode="outlined" onPress={handleSubmit(onSubmit)}>
         Sign in
       </Button>
-      <Button
-        style={{ marginTop: 10 }}
-        mode="text"
-        onPress={() => navigation.navigate('SignUp' as never)}>
+      <Button style={{ marginTop: 10 }} mode="text" onPress={() => navigation.navigate('SignUp' as never)}>
         Sign Up
       </Button>
     </View>
